95bbf1423200bf451e9f84b2e285714eacabb143
kavinrajsi <sikavinraj@gmail.com>
2023-03-06 17:01:12 +0530

    fixed menu as per design

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,18 @@
-$(".menu-button").click(function () {
-  $(".menu-button").toggleClass("open");
-  $("body").toggleClass("menu-active");
+$(".menu-button").click(function (e) {
+  e.stopPropagation();
+  $(".menu-button").addClass("open");
+  $("body").addClass("menu-active");
+  $("body").append('<div class="backdrop"></div>');
+
 });
+
+$(document.body).click( function(e) {
+  closeMenu();
+});
+
+function closeMenu(){
+  console.log('close button');
+  $(".menu-button").removeClass("open");
+  $("body").removeClass("menu-active");
+  $(".backdrop").remove();
+}
